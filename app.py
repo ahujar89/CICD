d23ef729d47c65974554a8ffb53a735929353905
Rishabh Ahuja <ahujar96@gmail.com>
2024-12-03 01:05:05 -0500

    [commit] - test change for pipeline trigger and create image om my dcker hub

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "This is a demo to run the CI/CD Pipeline!!"
+    return "This is a demo to run the CI/CD Pipeline!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
